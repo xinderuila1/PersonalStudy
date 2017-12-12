13ac44e2d85fcda002d3f22e3171ca25853c60a5
sunjj-a <898508832@qq.com>
2017-12-13 00:11:22 +0800

    提交进程ipc基础项目文件~

---
+++ Example/ProcessA/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <process.h>
+
+void main()
+{
+    printf("%s\n", "Hello world___A");
+    system("pause");
+    //     testing::InitGoogleTest(&argc, argv);
+    // 	return RUN_ALL_TESTS();
+}
